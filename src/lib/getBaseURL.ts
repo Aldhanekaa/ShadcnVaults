a2457165fdaf2e43ed82f16766434b7150565de5
Aldhanekaa <aldhanekadev@gmail.com>
2025-06-21 12:13:52 +0700

    fix the getBaseURL (2)

--- src/lib/getBaseURL.ts
+++ src/lib/getBaseURL.ts
@@ -3,9 +3,9 @@ export function getBaseURL(): string {
     return "http://localhost:3000";
   }
 
-  if (process.env.NEXT_PUBLIC_VERCEL_ENV === "preview") {
+  if (process.env.NEXT_PUBLIC_VERCEL_TARGET_ENV === "preview") {
     return `https://${process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL}`;
   }
 
-  return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`; // or your actual domain
+  return `https://${process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL}`; // or your actual domain
 }
